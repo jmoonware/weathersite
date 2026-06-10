f883d5c0aa3487ab9c6f813da02407416239c159
John Moon <moon.john.a@gmail.com>
2026-06-10 11:52:38 -0700

    split endpoints into microd and microf, with the ability to pre-justify left the forecast lines by a given amount

--- rest.py
+++ rest.py
@@ -44,3 +44,57 @@ def SetupRest(app):
 			if 'wind_angle' in readings:
 				filtered_readings['wind_angle']=readings['wind_angle']
 			return jsonify(filtered_readings | data.theDataReader.ephemera)
+		# just the forecast, which can be variable size
+		@app.route('/microf',methods=['GET'])
+		def get_latest_forecast():
+			args = request.args
+			width = 100
+			height = 100
+			if args.get('width'):
+				width = int(args.get('width'))
+			callbacks.update_all()
+			forecast_strings = {}
+			# collect the forecast strings; probably should be in a 
+			# separate dict eventually
+			# these can be longer than the screen width so inject line breaks 
+			# at the proper places
+			# the code on the microcontroller will keep track of the 
+			# number of lines
+			for key in data.theDataReader.ephemera:
+				if 'Forecast' in key:
+					forecast_strings[key] = data.theDataReader.ephemera[key]
+			# here we fix the text justification based on screen size
+			justified_strings = []
+			for fsk in forecast_strings:
+				toks = forecast_strings[fsk].split(' ')
+				justified_strings.append('')
+				cline=''
+				for itok,tok in enumerate(toks):
+					tcline = cline + ' ' + tok
+					if len(tcline) < width and itok + 1 != len(toks):
+						cline = tcline
+					else: # end line here
+						if itok + 1 == len(toks):
+							justified_strings[-1] = cline + ' ' +  tok
+						else:
+							justified_strings[-1] = cline 
+							cline = tok
+						justified_strings.append('')
+			return jsonify(justified_strings)
+		# just front screen values
+		@app.route('/microd',methods=['GET'])
+		def get_latest_data():
+			callbacks.update_all()
+			readings = data.theDataReader.GetLatestReadings() 
+			filtered_readings = {}
+			filtered_readings['wind_vmph']=None
+			if 'wind_vmph' in readings:
+				filtered_readings['wind_vmph']=readings['wind_vmph']
+			filtered_readings['wind_angle']=None
+			if 'wind_angle' in readings:
+				filtered_readings['wind_angle']=readings['wind_angle']
+			for key in data.theDataReader.ephemera:
+				if not 'Forecast' in key:
+					filtered_readings[key]=data.theDataReader.ephemera[key]
+			return jsonify(filtered_readings)
+
